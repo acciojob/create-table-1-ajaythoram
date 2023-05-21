e1887cfa6be7b6cd2c4463f1952d2c42d91c8890
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-21 14:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const tabel = document.getElementById("sampleTable");
+
+  const row = tabel.insertRow(0);
+  const tdata = row.insertCell(0);
+  const tdata2 = row.insertCell(1);
+  tdata.innerText = "New Cell1";
+  tdata2.innerText="NewCell2";
+  row.appendChild(tdata);
+  row.appendChild(tdata2); 
+ 
 }
